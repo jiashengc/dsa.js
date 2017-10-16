a09c96a5c11e7f70ecf21e71911e968127946cc9
Jia Sheng Chong <jiashengc@outlook.com>
2017-10-16 13:54:04 +1000

    CTCI Q1.1 improved

--- CTCI/arraysAndStrings/Q1.1.js
+++ CTCI/arraysAndStrings/Q1.1.js
@@ -6,17 +6,35 @@
 /**
  * Time Complexity: O(n^2)
  * 
- * @method uniqueCharacters
+ * @method uniqueCharacters1
  * @param {String} string
  * @return {boolean}
  */
-function uniqueCharacters(string) {
+function uniqueCharacters1(string) {
     for (let i = 0; i < string.length; i++) {
         for (let n = i + 1; n < string.length; n++) {
             if (string[i] === string[n]) {
                 return false;  
             }
         }
+    }
+    return true;
+}
+
+/**
+ * Time Complexity: O(n)
+ * 
+ * @method uniqueCharacters2
+ * @param {String} string 
+ * @return {boolean}
+ */
+function uniqueCharacters2(string) {
+    let map = {};
+    for (let i = 0; i < string.length; i++) {
+        if (map[string[i]]) {
+            return false;
+        } 
+        map[string[i]] = true;
     }
     return true;
 }
